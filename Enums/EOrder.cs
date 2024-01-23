a4ba7450f4b28b686ae9352bc1000adc5a371fda
Kuurama <74458092+Kuurama@users.noreply.github.com>
2024-01-23 22:36:40 +0100

    Add EOrder

---
+++ Enums/EOrder.cs
@@ -0,0 +1,16 @@
+﻿#if GUILDSABER_SERVER
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+#endif
+
+namespace GuildSaber.Enums;
+
+#if GUILDSABER_SERVER
+[JsonConverter(typeof(StringEnumConverter))]
+#endif
+public enum EOrder
+{
+    Desc,
+    Asc
+}
+
