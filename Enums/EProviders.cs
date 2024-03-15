dac80a66760a7c4bf7ecae9466ec560e1165ac9d
Kuurama <74458092+Kuurama@users.noreply.github.com>
2024-03-15 23:57:57 +0100

    Added missing Enums

---
+++ Enums/EProviders.cs
@@ -0,0 +1,15 @@
+﻿#if GUILDSABER_SERVER
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+#endif
+
+namespace GuildSaber.Enums;
+
+#if GUILDSABER_SERVER
+[JsonConverter(typeof(StringEnumConverter))]
+#endif
+public enum EProviders
+{
+    Discord    = 0,
+    BeatLeader = 1
+}
---
+++ Enums/ESorter.cs
@@ -0,0 +1,20 @@
+﻿#if GUILDSABER_SERVER
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+#endif
+
+namespace GuildSaber.Enums;
+
+#if GUILDSABER_SERVER
+[JsonConverter(typeof(StringEnumConverter))]
+#endif
+public enum ESorter
+{
+    Points     = 0,
+    Time       = 1,
+    EditTime   = 2,
+    Accuracy   = 3,
+    Score      = 4,
+    Rank       = 5,
+    Difficulty = 6
+}
